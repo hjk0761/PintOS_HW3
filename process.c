bf7633ff5649b0d3e16648bf162e86a9da121ff7
mhko1998 <80435528+mhko1998@users.noreply.github.com>
2022-05-15 22:54:33 +0900

    Update process.c

--- process.c
+++ process.c
@@ -87,9 +87,6 @@ process_execute (const char *file_name)
   char *fn_copy;
   tid_t tid;
   // ####### HW 3 #######
-  struct list_elem* e;
-  struct thread* t;
-
   /* Make a copy of FILE_NAME.
      Otherwise there's a race between the caller and load(). */
   fn_copy = palloc_get_page (0);
